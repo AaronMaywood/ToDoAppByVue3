5dd26358f9db267ed6f14cf5b061affb568aca6d
Aaron Maywood <am7274183@gmail.com>
2023-03-22 13:20:30 +0900

    コンポーネント側でリストを描画するようにした https://qiita.com/TakahiRoyte/items/5230b7b3685e0ab3756d の完了

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+	todos: Array,
+})
+</script>
+
+<template>
+  <p v-if="todos.length === 0">リストがまだありません</p>
+  <ul v-else v-for="t in todos">
+    <li>
+      <label v-bind:class="t.isComplete ? 'delete' : ''">
+        <input type="checkbox"
+          @click="checkUpdate"
+          v-model="t.isComplete"
+        >{{ t.message }}
+      </label>
+    </li>
+  </ul>
+</template>
+
+<style>
+.delete {
+	text-decoration: line-through;
+}
+</style>
