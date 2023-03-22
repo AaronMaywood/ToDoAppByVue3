12f65c02d74397367ddfc156898c234425de1dde
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2023-03-22 10:13:38 +0900

    ToDoを追加できるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 /* ToDoアプリ https://qiita.com/TakahiRoyte/items/a9aac2f069b99f3378c7 */
+<script setup>
+import { ref, reactive } from 'vue'
+const newTodo = ref('')
+const todos = reactive([
+  {
+    message:'VueでToDoアプリを作る',
+    isComplete: false,
+  }
+])
+
+const addTodo = function(){
+  todos.push({
+    message: newTodo.value,
+    isComplete: false,
+  })
+  newTodo.value = ''
+  console.log(todos)
+}
+</script>
 
 <template>
-  <button>追加</button>
   <button>完了済みを削除</button>
-  <form>
-    <input type="text">
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="newTodo">
+    <button>追加</button>
   </form>
   <ul>
     <li><input type="checkbox">ToDoアプリのプロトタイプを作成する</li>
