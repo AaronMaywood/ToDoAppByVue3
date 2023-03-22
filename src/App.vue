5021d6ad92a004e8fb6774df338c426050811136
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2023-03-22 09:49:24 +0900

    プロトタイプを作成

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,12 @@
 /* ToDoアプリ https://qiita.com/TakahiRoyte/items/a9aac2f069b99f3378c7 */
+
+<template>
+  <button>追加</button>
+  <button>完了済みを削除</button>
+  <form>
+    <input type="text">
+  </form>
+  <ul>
+    <li><input type="checkbox">ToDoアプリのプロトタイプを作成する</li>
+  </ul>
+</template>
