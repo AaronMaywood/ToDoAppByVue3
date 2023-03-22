06b9fedae407739cc14173658d83db503f2f2078
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2023-03-22 11:09:33 +0900

    要件整理のため、残作業をToDoとして追加 https://qiita.com/TakahiRoyte/items/a9aac2f069b99f3378c7 をもとに要件を起こした

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,29 @@ import { ref, reactive } from 'vue'
 const newTodo = ref('')
 let todos = ref([
   {
-    message:'ToDoアプリのプロトタイプを作る',
+    message:'初期表示でデータが何もない時、リストの位置に「ToDoがまだありません！」と表示する',
+    isComplete: false,
+  },
+  {
+    message:'フォームに文字を入力し、追加ボタンを押すと文字列を元にToDoリストに追加される',
     isComplete: true,
   },
   {
-    message:'VueでToDoアプリを作る',
+    message:'ToDoに追加したタイミングでフォームの文字列はクリアされる',
+    isComplete: true,
+  },
+  {
+    message:'フォームに文字が未入力時に追加ボタンを押しても、アラートが表示されリストに追加されない',
     isComplete: false,
-  }
+  },
+  {
+    message:'ToDo毎のチェックボックスのオンオフで文字列に完了済みのラインの切り替えができる',
+    isComplete: false,
+  },
+  {
+    message:'完了済みを削除するボタンを押すとチェックボックスがオンになっているToDoが削除される',
+    isComplete: true,
+  },
 ])
 
 const addTodo = function(){
@@ -19,12 +35,12 @@ const addTodo = function(){
     isComplete: false,
   })
   newTodo.value = ''
-  console.log(todos)
 }
 const checkUpdate = function(e){
   console.log(e.target)
   // checked情報は↓のようにして取得できる
   console.log(e.target.checked)
+  // どうやって自身の状態をアップデートしたらいい？
 }
 
 const deleteCompleted = () => {
