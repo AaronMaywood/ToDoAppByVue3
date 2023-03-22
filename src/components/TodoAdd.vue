0a32a4d70e25a108009e291658bf9c12db9a02c4
Aaron Maywood <am7274183@gmail.com>
2023-03-22 12:57:45 +0900

    ボタンをコンポーネントに切り分けた

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue'
+import MyButton from './basics/MyButton.vue'
+
 const newTodo = ref('')
 
 const emit = defineEmits(['delete-done','add-todo'])
@@ -7,13 +9,14 @@ const emit = defineEmits(['delete-done','add-todo'])
 const addTodo = (e) => {
 	emit('add-todo', newTodo)
 }
-
 </script>
 
 <template>
-  <button type="button" @click="$emit('delete-done')">完了済みを削除</button>
+  <MyButton @click="$emit('delete-done')">
+	完了済みを削除
+  </MyButton>
   <form @submit.prevent="addTodo">
     <input type="text" v-model="newTodo">
-    <button>追加</button>
+	<MyButton>追加</MyButton>
   </form>
 </template>
